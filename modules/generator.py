f572a5f38cd3e0394b3ff596c26e484c9082eae5
Lizazavr <78729103+Lizazavr@users.noreply.github.com>
2024-02-12 13:53:32 +0300

    модуль создания и запуска генератора

---
+++ modules/generator.py
@@ -0,0 +1,14 @@
+import time
+import scripts
+import p_mqtt
+
+flag = False
+
+# создание и запуск нового генератора
+def generator(name):
+    global flag
+
+    while(flag):
+        test_data = scripts.open_script(name)
+        x = p_mqtt.run(test_data)
+        time.sleep(1)
